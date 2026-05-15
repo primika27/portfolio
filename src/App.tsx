64989daf3de4b4c7743ef51e61ae996020b6c388
primika27 <kprimika@yahoo.com>
2026-05-15 14:19:06 -0400

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>  
+      <div>
+        <h1>Welcome to Primika's Portfolio!</h1>
+        <p>Hi there! I'm Primika, a passionate software developer with a love for creating innovative solutions. This portfolio showcases my projects, skills, and experiences in the world of technology. Feel free to explore and get in touch if you'd like to collaborate or learn more about my work!</p>
+      </div>
+      <div>
+        <h2>Projects</h2>
+        <ul>
+          <li><strong>Project A:</strong> A web application that simulates a trading platform with event-driven design
+          {/* <link href="https://github.com/primika/trading-platform" target="_blank" rel="noopener noreferrer"> (GitHub Repository)</link> */}
+          </li>
+          <li><strong>Project B:</strong> A password Vault that explores secure password management techniques
+          {/* <link href="https://github.com/primika/password-vault" target="_blank" rel="noopener noreferrer"> (GitHub Repository)</link> */}
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>Skills</h2>
+        <ul>
+          <li>JavaScript</li>
+          <li>Java</li>
+          <li>Committed</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
